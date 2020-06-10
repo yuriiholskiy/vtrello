f8f526706b557200f936600cee3fce2e9df8f68a
Yurii Holskyi <yurii.golskyi@gmail.com>
2020-06-10 19:54:27 +0300

    deploy

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,25 @@
 const purgecss = require('@fullhuman/postcss-purgecss')({
   content: ['./src/**/*.html', './src/**/*.vue'],
 
-  defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || []
+  defaultExtractor: (content) => {
+    const broadMatches = content.match(/[^<>"'`\s]*[^<>"'`\s:]/g) || [];
+
+    const innerMatches = content.match(/[^<>"'`\s.()]*[^<>"'`\s.():]/g) || [];
+
+    return broadMatches.concat(innerMatches);
+  },
+  whitelistPatterns: [
+    /-(leave|enter|appear)(|-(to|from|active))$/,
+    /^(?!(|.*?:)cursor-move).+-move$/,
+    /^router-link(|-exact)-active$/,
+    /data-v-.*/
+  ]
 });
 
 module.exports = {
-  plugins: {
-    tailwindcss: './tailwind.config.js',
-    autoprefixer: {},
+  plugins: [
+    require('tailwindcss')('./tailwind.config.js'),
+    require('autoprefixer'),
     ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
-  }
+  ]
 };
